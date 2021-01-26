c553059772f9fde526f784771fbe6d78ff594306
MatKie <mtth.kiesel@gmail.com>
2021-01-26 10:00:41 +0100

    Add first parameter variation: k_ij(CM/T - W) = 0.25

---
+++ update_ff.py
@@ -0,0 +1,18 @@
+# +
+from mkutils.gromacs_writer import FFWriter
+import json, os
+
+ff_file = 'cc_parameters.json'
+# -
+
+ff = FFWriter(ff_file)
+
+# Update Crossints
+ff.add_crossint('Wift25', 'CT', k=0.25, update=True)
+ff.add_crossint('Wift25', 'CM', k=0.25, update=True)
+
+# No need to write tables again, because l_r, l_a did not change
+#ff.write_tables(shift=True, cutoff=2.0)
+ff.write_forcefield(outfile='ffnonbonded_v1.itp')
+
+
