3b0d9fdd24793046dad953079bab3e518ef7bae2
MatKie <mtth.kiesel@gmail.com>
2021-01-26 10:02:18 +0100

    Add second parameter variation: k_ij(CM/T - W) = 0.4)

--- update_ff.py
+++ update_ff.py
@@ -15,4 +15,11 @@ ff.add_crossint('Wift25', 'CM', k=0.25, update=True)
 #ff.write_tables(shift=True, cutoff=2.0)
 ff.write_forcefield(outfile='ffnonbonded_v1.itp')
 
+# Further update crossints
+ff.add_crossint('Wift25', 'CT', k=0.4, update=True)
+ff.add_crossint('Wift25', 'CM', k=0.4, update=True)
+
+# No need to write tables again, because l_r, l_a did not change
+#ff.write_tables(shift=True, cutoff=2.0)
+ff.write_forcefield(outfile='ffnonbonded_v2.itp')
 
